5ecae3696099b332e2cced5af7659e77bf9f45fe
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2020-07-24 10:29:52 -0400

    Ok

--- components/pages/disfunctional-family/glenda.jsx
+++ components/pages/disfunctional-family/glenda.jsx
@@ -26,6 +26,21 @@ export default function Glenda() {
             <PicGallery images={images} />
           </div>
         </div>
+
+        <div className="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center mt-6">
+          <p className="leading-relaxed text-lg">
+            Mi niña feliz cumpleaños!!!!!! Que este y cada año de tu vida venga
+            cargado de cosas hermosas para ti que tanto lo mereces! Gracias por
+            compartir toda esa dulzura y bondad infantil, que tanto te
+            caracteriza, con nosotros: tus amigos, tu familia! Nosotros quienes
+            estamos dispuestos a bajar las estrellas para ti si es necesario,
+            porque te lo mereces y eres un bello ser humano! 22s años más de
+            alegría y momentos felices porque lo demás viene solo y lo atraes tú
+            con tanta energía positiva y lindas palabras!!!
+          </p>
+          <p>Te quiero siempre</p>
+          <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-8 mb-6"></span>
+        </div>
       </div>
     </section>
   );
